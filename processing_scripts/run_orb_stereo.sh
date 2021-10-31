7142c9ee4a55af42aef76449ce2dc748e8f4802d
Niclas Zeller <niclas@artisense.ai>
2021-10-31 21:22:44 +0100

    update run_orb_stereo.sh to ORB-SLAM3
    Signed-off-by: Niclas Zeller <niclas@artisense.ai>

--- processing_scripts/run_orb_stereo.sh
+++ processing_scripts/run_orb_stereo.sh
@@ -2,18 +2,18 @@
 # and adapt the following paths respectively
 
 FOUR_SEASONS_TOOLS_DIR=~/workspace/4seasons_tools
-ORBSLAM_DIR=~/workspace/ORB_SLAM2
+ORBSLAM_DIR=~/workspace/ORB_SLAM3
 BOW_VOCABULARY=$ORBSLAM_DIR/Vocabulary/ORBvoc.txt
-ORBSLAM_CALIB_FILE=$FOUR_SEASONS_TOOLS_DIR/configs/orb_slam/orb_slam_calib.yaml
+ORBSLAM_CALIB_FILE=$FOUR_SEASONS_TOOLS_DIR/configs/orb_slam/orb_slam_stereo_calib.yaml
 
 for d in *; do
     if [ -d $d ]; then
         echo "$d"
 	if [ ! -f $d/odometry_resutls/orb_slam_stereo.txt ]; then
-	    $ORBSLAM_DIR/Examples/Artisense/stereo_artisense \
+	    $ORBSLAM_DIR/Examples/4seasons/4seasons_runner \
                 $BOW_VOCABULARY \
                 $ORBSLAM_CALIB_FILE \
-                $d 
+                $d
 	    if [ ! -d $d/odometry_resutls ]; then
                 mkdir $d/odometry_resutls
             fi
