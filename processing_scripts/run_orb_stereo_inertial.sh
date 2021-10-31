e99b6186a15cdad371f67f4ce1927f75e5375918
Niclas Zeller <niclas@artisense.ai>
2021-10-31 21:23:14 +0100

    add stereo-inertial ORB-SLAM
    Signed-off-by: Niclas Zeller <niclas@artisense.ai>

---
+++ processing_scripts/run_orb_stereo_inertial.sh
@@ -0,0 +1,24 @@
+# to run the this script copy it to the folder containing the sequences to be processed
+# and adapt the following paths respectively
+
+FOUR_SEASONS_TOOLS_DIR=~/workspace/4seasons_tools
+ORBSLAM_DIR=~/workspace/ORB_SLAM3
+BOW_VOCABULARY=$ORBSLAM_DIR/Vocabulary/ORBvoc.txt
+ORBSLAM_CALIB_FILE=$FOUR_SEASONS_TOOLS_DIR/configs/orb_slam/orb_slam_stereo_calib.yaml
+
+for d in *; do
+    if [ -d $d ]; then
+        echo "$d"
+	if [ ! -f $d/odometry_resutls/orb_slam_stereo_inertial.txt ]; then
+	    $ORBSLAM_DIR/Examples/4seasons/4seasons_runner \
+                $BOW_VOCABULARY \
+                $ORBSLAM_CALIB_FILE \
+                $d \
+		"with_imu"
+	    if [ ! -d $d/odometry_resutls ]; then
+                mkdir $d/odometry_resutls
+            fi
+            mv CameraTrajectory.txt $d/odometry_resutls/orb_slam_stereo_inertial.txt
+        fi
+    fi
+done
